50f2966318a2bc7352e749c2dcdb7e4008586889
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-05-18 23:40:49 +0530

    fix: middleware - redirect to dashboard if user already logged in

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,26 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
+    afterAuth(auth, req, evt) {
+        // Handle users who aren't authenticated
+        if (!auth.userId && !auth.isPublicRoute) {
+            return redirectToSignIn({ returnBackUrl: req.url });
+        }
+        // Redirect signed in users to organization selection page if they are not active in an organization
+        if (
+            auth.userId &&
+            req.nextUrl.pathname === "/"
+        ) {
+            return NextResponse.rewrite(new URL('/dashboard', req.url))
+        }
+        // If the user is signed in and trying to access a protected route, allow them to access route
+        if (auth.userId && !auth.isPublicRoute) {
+            return NextResponse.next();
+        }
+        // Allow users visiting public routes to access them
+        return NextResponse.next();
+    },
     publicRoutes: ["/"],
 });
 
